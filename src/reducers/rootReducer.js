0247ae9282fa779ba9d655bf69ccf698e1601ec2
varandej <vladislawladislav@rambler.ru>
2019-03-04 18:02:22 +0300

    with flow

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,10 +2,11 @@
 
 import { combineReducers } from 'redux';
 import typehead from './typehead';
+import type { Action } from '../types/Action';
 
 const reducers = { typehead };
 
 export type Reducers = typeof(reducers);
 
-export const rootReducer = combineReducers(reducers);
+export const rootReducer = combineReducers<Object, Action>(reducers);
 
