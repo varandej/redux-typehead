0901d22908ff45054742952cba3bcd369c51cda1
varandej <vladislawladislav@rambler.ru>
2019-03-04 17:26:54 +0300

    with flow and one problem

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './app';
+import { store } from './store/configureStore.js';
+import './styles/app.scss';
+
+ReactDOM.render(
+    <div>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>  
+            </BrowserRouter>
+        </Provider>
+    </div>,
+    document.getElementById("root")
+);
